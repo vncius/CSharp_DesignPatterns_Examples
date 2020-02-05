eb9199b8f4b1e54df3fa8ece3830a86e391b9c65
Vinicius Vieira Abreu <v.vieira.go@gmail.com>
2020-02-05 14:59:37 -0300

    Adicionado padrão de criação -> Singleton

---
+++ Padrao_Singleton/Model/Cadeira.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Padrao_Singleton.Model
+{
+    /// <summary>
+    /// Classe no modelo Singleton simples
+    /// </summary>
+    public sealed class Cadeira
+    {
+        private static readonly Cadeira instance = new Cadeira();
+        public int Quantidade { get; set; }
+ 
+        private Cadeira() { }
+ 
+        public static Cadeira Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
---
+++ Padrao_Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Padrao_Singleton.Model;
+
+namespace Padrao_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary> 
+            /// CHAMADA SINGLETON SIMPLES
+            /// </summary>
+            Cadeira _cadeira1 = Cadeira.Instance;
+            _cadeira1.Quantidade = 1;
+ 
+            Cadeira _cadeira2 = Cadeira.Instance;
+            _cadeira2.Quantidade = 5;
+ 
+            Console.WriteLine(_cadeira1.Quantidade.ToString());
+            Console.WriteLine(_cadeira2.Quantidade.ToString());
+            Console.ReadLine();
+        }
+    }
+}
